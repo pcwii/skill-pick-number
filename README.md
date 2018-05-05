ef1ebcc6987733b7bb6b971aa8d2e376db751a41
pcwii <30759206+pcwii@users.noreply.github.com>
2018-05-05 18:31:54 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,16 +1,30 @@
-# skill-pick-number
-Ask Mycroft.AI to pick a number between a min and max value
-## Description 
-Mycroft.AI will select a number based on a range you request.
-## Examples 
-* "pick a number between 1 and 100"
-* "choose a number from 1 to 10"
-* "select a number between 100 and 90"
+## coin flip
+This skill flips a virtual coin
+
+## Description
+Flip a virtual coin by saying "hey Mycroft, flip a coin".
+It will then randomly choose between heads and tails.
+
+To get this done we need
+  - A sound effect for the coin flip
+  - Randomly pick between heads or tails
+  - Speak the result
+
+
+## Current state
+
+Working features:
+ - Flipping a coin
+
+ ## Sound effect
+
+ The sound effect was created by Mattias Michael Lahoud.
+ It is available under the Creative Commons Attribution 3.0 Unported (CC BY 3.0)
+ license.
+ https://freesound.org/people/Mattix145/
+
+## Examples
+* "flip a coin"
+
 ## Credits
-PCWii
-## Require 
-platform_picroft 
-## Other Requirements
-- [Mycroft](https://docs.mycroft.ai/installing.and.running/installation)
-## Further Reading
-- 
+Willem Ligtenberg <willem@wligtenberg.nl> @wligtenberg
---
+++ __init__.py
@@ -0,0 +1,82 @@
+# Copyright 2017 Willem Ligtenberg
+#
+# Coin flip skill is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Coin flip skill is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Mycroft Core.  If not, see <http://www.gnu.org/licenses/>.
+
+from os.path import dirname, join
+
+from adapt.intent import IntentBuilder
+from mycroft.skills.core import MycroftSkill
+from mycroft.util.log import getLogger
+from mycroft.util import play_mp3
+import re
+import random
+
+__author__ = 'PCWii'
+
+# Logger: used for debug lines, like "LOGGER.debug(xyz)". These
+# statements will show up in the command line when running Mycroft.
+LOGGER = getLogger(__name__)
+
+# The logic of each skill is contained within its own class, which inherits
+# base methods from the MycroftSkill class with the syntax you can see below:
+# "class ____Skill(MycroftSkill)"
+class PickNumberSkill(MycroftSkill):
+
+    # The constructor of the skill, which calls MycroftSkill's constructor
+    def __init__(self):
+        super(PickNumberSkill, self).__init__(name="PickNumberSkill")
+
+    # This method loads the files needed for the skill's functioning, and
+    # creates and registers each intent that the skill uses
+    def initialize(self):
+        self.load_data_files(dirname(__file__))
+        pick_number_intent = IntentBuilder("PickNumberIntent").\
+            require("PickKeyword").\
+            require("NumberKeyword").\
+            require("BetweenKeyword").\
+            require("AndKeyword").build()
+        self.register_intent(pick_number_intent, self.handle_pick_number_intent)
+
+    # The "handle_xxxx_intent" functions define Mycroft's behavior when
+    # each of the skill's intents is triggered: in this case, he simply
+    # speaks a response. Note that the "speak_dialog" method doesn't
+    # actually speak the text it's passed--instead, that text is the filename
+    # of a file in the dialog folder, and Mycroft speaks its contents when
+    # the method is called.
+    def handle_pick_number_intent(self, message):
+        strRemainder = str(message.utterance_remainder())
+        strLimits = re.findall('\d+',strRemainder)
+        intFirst = int(strLimits[0])
+        intSecond = int(strLimits[1])
+        if intFirst < intSecond:
+            lowNumber = intFirst
+            highNumber = intSecond
+        else:
+            lowNumber = intSecond
+            highNumber = intFirst
+        myNumber = random.randint(lowNumber,highNumber)
+        self.speak_dialog("pick.number", data={"number": str(myNumber)})
+        #self.speak("The number I have selected is " + str(myNumber))
+
+    # The "stop" method defines what Mycroft does when told to stop during
+    # the skill's execution. In this case, since the skill's functionality
+    # is extremely simple, the method just contains the keyword "pass", which
+    # does nothing.
+    def stop(self):
+        pass
+
+# The "create_skill()" method is used to create an instance of the skill.
+# Note that it's outside the class itself.
+def create_skill():
+    return PickNumberSkill()
